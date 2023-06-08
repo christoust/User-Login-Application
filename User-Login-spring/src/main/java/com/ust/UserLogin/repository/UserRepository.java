f52b22b4ef43ad68c974197d7237fa42d4b4d98d
Christo <245052@ust.com>
2023-06-08 09:33:01 +0530

    first commit

---
+++ User-Login-spring/src/main/java/com/ust/UserLogin/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.ust.UserLogin.repository;
+
+import com.ust.UserLogin.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,java.lang.String> {
+}
---
+++ userlogin-angular/src/app/model/user.spec.ts
@@ -0,0 +1,7 @@
+import { User } from './user';
+
+describe('Auth', () => {
+  it('should create an instance', () => {
+    expect(new User()).toBeTruthy();
+  });
+});
